13bb346b83dbe607ce0800209764755308161311
Azizatul Mashwafah <75109998+azizatulmashwafah2601@users.noreply.github.com>
2023-01-04 20:40:53 +0700

    update tampilan

--- Responsive/public/index.php
+++ Responsive/public/index.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
@@ -75,27 +76,48 @@
 
     </script>
 
-	<img class="wave" src="img/17545.jpg">
+<img class="wave" src="img/17545.jpg">
 	<div class="container">
 		<div class="img">
 			<img src="img/thermometer-snow.svg">
 		</div>
-		<div class="login-content">
-			<form>
-			<h3 class="title">Monitoring Suhu</h3>
-           		<div class="input-div one">
-				<h5>Kelembapan <div id="postElementHumid"></div> %</h5> 
-				<h5>Temperatur <div id="postElementTemp"></div> C</h5>
-			</div>
-				
-			</form>
-			<div>
-            	<h3>Status Mesin : <div id="Key"></div></h3>
-				<button onclick="ledOn()" class="btn">Hidupkan Pengering</button>
-				<button onclick="ledOff()" class="btn">Matikan Pengering</button>
-            </div>
-            	</div>
-				
+        
+		<div class="monitoring-container">
+            <form action="">
+                <div class="mb-3 row">             
+                    <div class="mb-3 row">
+                        <h3 class="col-sm-2 col-form-label">Monitoring Suhu</h3>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Kelembapan</label>
+                        <div class="col-sm-10">
+                            <label class="col-sm-2 col-form-label" id="postElementHumid"></label>
+                            <label class="">%</label>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Temperatur</label>
+                        <div class="col-sm-10">
+                            <label class="col-sm-2 col-form-label" id="postElementTemp"></label>
+                            <label>C</label>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <h3 class="col-sm-4 col-form-label">Status Mesin</h3>
+                    </div>
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Status</label>
+                        <div class="col-sm-10">
+                            <label>:</label>
+                            <label class="col-sm-2 col-form-label" id="Key"></label>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <button onclick="ledOn()" class="btn btn-primary" style="border-radius: 50px;width: 55%;height: 55px;">Hidupkan Pengering</button>
+                        <button onclick="ledOff()" class="btn btn-primary" style="border-radius: 50px;width: 55%;height: 55px;">Matikan Pengering</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </body>
